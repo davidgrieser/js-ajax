a925d26829b552f3ed8d2e19eaffb0d4c0f81778
David Grieser <david.grieser@galvanize.com>
2021-08-06 08:51:50 -0500

    myFetch wrapped promise

--- main.js
+++ main.js
@@ -1,8 +1,30 @@
+function myFetch(url) {
+    return new Promise((resolve, reject) => {
+        let fetchPromise = fetch(url);
+        fetchPromise.then((response) => {
+            if(response.status < 400) {
+                resolve(response);
+            } else {
+                reject(response);
+            }
+        })
+    });
+}
 document.querySelector('#fetch').addEventListener('click', () => {
-    fetch('https://api.github.com/users/microsoft')
-        .then((data) => {
+    document.querySelector('#output').innerHTML = "";
+    document.querySelector('#output').style.color = "";
+    let getGithubPromise = myFetch(`https://api.github.com/users/${document.querySelector('#username').value}`)
+    getGithubPromise.then((data) => {
+        console.log(getGithubPromise);
+        console.log(data)
             return data.json();
-        }).then((json) => {
+        })
+        .then((json) => {
             document.querySelector('#output').innerHTML = JSON.stringify(json) ;
-        });
+        })
+        .catch((errorResponse) => {
+            document.querySelector('#output').innerHTML = "Fetch Failed";
+            document.querySelector('#output').style.color = "red";
+            console.error(errorResponse);
+        })
 })
