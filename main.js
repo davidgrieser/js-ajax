e6c57cee3a77d36ff4e732f31f87f67eca2ba253
David Grieser <david.grieser@galvanize.com>
2021-08-06 10:09:45 -0500

    renamed things

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 // Wrap Fetch API so we get a fullfiled(resolve) OR rejected Promise
-function myFetch(url) {
+function fetchWithResolveReject(url) {
     // Should return a Promise
     return new Promise((resolve, reject) => {
         // Call fetch()
@@ -15,21 +15,21 @@ function myFetch(url) {
     })
 }
 
-const handleError = (pizza) => {
+const handleError = (errorResponse) => {
     document.querySelector('#output').innerHTML = "Fetch Failed";
     document.querySelector('#output').style.color = "red";
-    console.error(pizza);
+    console.error(errorResponse);
 }
 
 document.querySelector('#fetch').addEventListener('click', () => {
     document.querySelector('#output').innerHTML = "";
     document.querySelector('#output').style.color = "";
     const url = `https://api.github.com/users/${document.querySelector('#username').value}`;
-    let getGithubPromise = myFetch(url)
-    getGithubPromise.then((data) => {
+    let getGithubPromise = fetchWithResolveReject(url)
+    getGithubPromise.then((response) => {
         console.log(getGithubPromise);
-        console.log(data)
-        return data.json();
+        console.log(response)
+        return response.json();
     }).then((json) => {
         document.querySelector('#output').innerHTML = JSON.stringify(json);
     }).catch(handleError)
